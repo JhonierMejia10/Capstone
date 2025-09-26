59d857d209af294c2763c8a5f5c048d02e8e3595
JhonierMejia10 <jhonimejiajaramillo@gmail.com>
2025-09-26 14:37:40 -0500

    Creating tables Booking and Menu

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = 'CapstoneProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemonCapstone',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER':'root',
+        'PASSWORD':'admin',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
