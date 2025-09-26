8666ce94decdb8f90495cd4ccd70cf29b4b67d53
JhonierMejia10 <jhonimejiajaramillo@gmail.com>
2025-09-26 12:38:11 -0500

    endpoint de prueba

--- CapstoneProject/settings.py
+++ CapstoneProject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'CapstoneProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Asegúrate de que esto apunte a la carpeta static
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
